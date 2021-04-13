0d9cc8bdcd9f6b2363996956df7cc3c0a2bd29e4
Aman Singh <aman.singh_cs.aiml19@gla.ac.in>
2021-04-13 23:32:36 +0530

    main function code is completed

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -4,5 +4,17 @@ public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
         // and perform insert, search, traversal, delete
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(20);
+        tree.insert(60);
+        tree.insert(35);
+        tree.insert(59);
+        tree.insert(32);
+        tree.inOrder(tree.getRoot());
+        tree.insert(20);
+        tree.inOrder(tree.getRoot());
+        System.out.println(tree.search(69));
+        System.out.println(tree.search(42));
     }
 }
