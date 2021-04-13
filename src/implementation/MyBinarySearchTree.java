ac5521cfc52cf284f8ac5a69619827f7c6963de8
Aman Singh <aman.singh_cs.aiml19@gla.ac.in>
2021-04-13 23:18:15 +0530

    insert code is completed

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,9 +5,42 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    private Node root;
+    private int size;
+
+    public MyBinarySearchTree() {
+        root = null;
+        size = 0;
+    }
 
     @Override
     public void insert(E data) {
+        implementation.Node<E> node = new implementation.Node<>(data);
+        if(isEmpty()){
+            root = node;
+        }
+        else{
+            Node<E> temp = root;
+            Node<E> parent = null;
+            while(temp!=null){
+                parent = temp;
+                if(data.compareTo(temp.getData())<=0){
+                    temp = temp.getLeft();
+                }
+                else{
+                    temp = temp.getRight();
+                }
+            }
+            if(data.compareTo(parent.getData())<=0){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
+
+        }
+        size++;
+        System.out.println("Node inserted");
 
     }
 
